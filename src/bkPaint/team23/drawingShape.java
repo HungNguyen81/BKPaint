bdff646fd5a4c014b1ee3f4a9b14d042bb231fb8
utngo007 <trungdeptrai2002>
2020-06-09 17:31:33 +0700

    add class drawingShape

--- src/bkPaint/team23/drawingShape.java
+++ src/bkPaint/team23/drawingShape.java
@@ -15,35 +15,6 @@ public class drawingShape extends drawing {
 
     public static boolean isPressed = false;
 
-    MouseListener createShapePress = new MouseAdapter() {
-        @Override
-        public void mousePressed(MouseEvent e) {
-            oldX = e.getX();
-            oldY = e.getY();
-            graphic2d.setStroke(new BasicStroke(penSize, BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL));
-            graphic2d.setPaint((isColorChooser)? color:Color.getColor("myColor"));
-            isPressed = false;
-            bfImg[index] = copyImage((BufferedImage) image);
-            frameArr[index] = numFrame;                 // lưu lại từng frame và chỉ số
-            if(index == MAX_UNDO - 1){                  // nếu số lượt undo đạt giới hạn
-                for(int i = 0; i < MAX_UNDO - 1; i++){
-                    bfImg[i] = copyImage(bfImg[i + 1]);
-                    frameArr[i] = frameArr[i + 1];
-                }
-            } else { index++; }
-
-        }
-    };
-    MouseListener getCreateShapeDrag = new MouseAdapter() {
-        @Override
-        public void mouseDragged(MouseEvent e) {
-            if(isPressed) Undo(true);
-            curX = e.getX();
-            curY = e.getY();
-            graphic2d.drawLine(curX, curY, oldX, oldY);
-            isPressed = true;
-        }
-    };
 
         public static void createShape(Point oldPos, Point curPos) {
             isShape = true;
